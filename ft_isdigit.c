0d2a054aa31045957e457d801299c9900e30a8fd
root <root@DESKTOP-QEVI5GA.localdomain>
2020-12-29 16:50:26 +0900

    libft

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoh <hnoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/28 12:03:23 by hnoh              #+#    #+#             */
-/*   Updated: 2020/12/29 16:40:33 by hnoh             ###   ########.fr       */
+/*   Created: 2020/12/29 16:38:11 by hnoh              #+#    #+#             */
+/*   Updated: 2020/12/29 16:39:16 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
-#include <string.h>
-
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int 		ft_isprint(int c);
-
-#endif
+int		ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoh <hnoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/28 12:03:23 by hnoh              #+#    #+#             */
-/*   Updated: 2020/12/29 16:40:33 by hnoh             ###   ########.fr       */
+/*   Created: 2020/12/29 16:42:48 by hnoh              #+#    #+#             */
+/*   Updated: 2020/12/29 16:43:33 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
-#include <string.h>
-
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int 		ft_isprint(int c);
-
-#endif
+int		ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
