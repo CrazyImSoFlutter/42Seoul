ce893c1d4453c4a646dfb22e6f79bf3f6b7a9aea
노근(hnoh) <nogeun@sinjunho.localdomain>
2021-01-07 13:07:15 +0900

    fix

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,56 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/04 11:14:33 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/04 11:40:44 by nogeun           ###   ########.fr       */
+/*   Created: 2021/01/07 12:58:30 by hnoh              #+#    #+#             */
+/*   Updated: 2021/01/07 13:05:40 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		ft_count(long int i)
+void		ft_convert(char *str, int cnt, int n)
 {
-	int		count;
+	long long	i;
 
-	count = 0;
-	if (i < 0)
-	{
-		i *= -1;
-		count++;
-	}
-	while (i > 0)
-	{
-		i /= 10;
-		count++;
-	}
-	return (count);
-}
-
-char			*ft_itoa(int n)
-{
-	char		*str;
-	int			i;
-	long int	nb;
-
-	nb = n;
-	i = ft_count(nb);
-	if (!(str = malloc(i * sizeof(char) + 1)))
-		return (0);
-	str[i--] = 0;
-	if (nb == 0)
-	{
-		str = ft_calloc(2, sizeof(char));
-		str[0] = 48;
-	}
-	if (nb < 0)
+	i = n;
+	if (i < n)
 	{
 		str[0] = '-';
-		nb = nb * -1;
+		i *= -1;
 	}
-	while (nb > 0)
+	str[cnt] = '\0';
+	while (1)
 	{
-		str[i--] = nb % 10 + '0';
-		nb = nb / 10;
+		str[cnt - 1] = i % 10 + '0';
+		i = i / 10;
+		if (!i)
+			break ;
+		cnt--;
 	}
+}
+
+char		*ft_itoa(int n)
+{
+	char		*str;
+	int			cnt;
+	int			i;
+
+	i = n;
+	cnt = 1;
+	if (i < 0)
+		cnt = 2;
+	while (1)
+	{
+		if (!(i = i / 10))
+			break ;
+		cnt++;
+	}
+	if (!(str = (char *)malloc(sizeof(char) * (cnt + 1))))
+		return (NULL);
+	ft_convert(str, cnt, n);
 	return (str);
 }
