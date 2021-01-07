93b14791f3db447ea5a5f6dd4e25050a37e13132
노근(hnoh) <nogeun@sinjunho.localdomain>
2021-01-07 13:27:22 +0900

    fix

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,33 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/07 13:10:10 by hnoh              #+#    #+#             */
-/*   Updated: 2021/01/07 13:14:05 by hnoh             ###   ########.fr       */
+/*   Created: 2021/01/07 13:23:56 by hnoh              #+#    #+#             */
+/*   Updated: 2021/01/07 13:27:01 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int		ft_get_digit_count(long int n)
+{
+	int		i;
+
+	if (n < 0)
+		n *= -1;
+	i = 1;
+	while (n > 9)
+	{
+		i++;
+		n /= 10;
+	}
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
-	char *str;
+	char			*str;
+	int				digit_count;
+	long int		long_n;
 
-	if (!(str = (char *)malloc(sizeof(char) * 2)))
+	long_n = n;
+	digit_count = ft_get_digit_count(long_n);
+	if (long_n < 0)
+		digit_count++;
+	if (!(str = malloc((digit_count + 1) * sizeof(char))))
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(str, "-2147483648"));
-	if (n < 0)
+	str[0] = '0';
+	if (long_n < 0)
 	{
 		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		long_n *= -1;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
+	str[digit_count] = '\0';
+	while (((digit_count >= 0 && str[0] != '-')
+				|| (digit_count > 0 && str[0] == '-')) && long_n != 0)
 	{
-		str[0] = n + '0';
-		str[1] = '\0';
+		str[digit_count - 1] = (long_n % 10) + '0';
+		long_n /= 10;
+		digit_count--;
 	}
 	return (str);
 }
